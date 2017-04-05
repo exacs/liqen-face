692f8555b6dfe2e3138f45486b3ccaf15e486294
Carlos Saito <c.saito.style@gmail.com>
2017-04-05 12:27:07 +0200

    Ultra-simplify the server

---
+++ README.md
@@ -0,0 +1,7 @@
+# Liqen Front-end
+
+## To start
+
+```
+npm run start
+```
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 /**
- * Script that serves as entry point to build the Server in Production mode.
+ * Script that serves as entry point for the Server
  */
-import http from 'http'
-import httpServer from './server/http'
-
+const express = require('express')
+const app = express()
+const path = require('path')
+const http = require('http')
 const PORT = process.env.PORT || 3000
-const app = httpServer()
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+app.use('/static', express.static('public'))
+
 const server = http.Server(app)
 
 server.listen(PORT, function () {
