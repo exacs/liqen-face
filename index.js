848d2cfa90d619e3ed095762c5a59d759c584cf2
Carlos Saito <c.saito.style@gmail.com>
2017-04-07 10:38:33 +0200

    Add /how, /dashboard and /questions pages

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ const PORT = process.env.PORT || 3000
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
+
+app.get('/how', (req, res) => {
+  res.sendFile(path.join(__dirname, 'how.html'))
+})
+
+app.get('/questions', (req, res) => {
+  res.sendFile(path.join(__dirname, 'questions.html'))
+})
+
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dashboard.html'))
+})
+
+
 app.use('/static', express.static('public'))
 
 const server = http.Server(app)
