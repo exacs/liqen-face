c0bd80efa93685344e59e62457cf8f2d6b47cda6
Carlos Saito <c.saito.style@gmail.com>
2017-05-01 23:38:16 +0200

    Drop SSR

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from 'express'
 import path from 'path'
 import http from 'http'
 import router from './router'
-import dashboard from './dashboard'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -29,9 +28,6 @@ app.use('/static', express.static('public'))
 
 // Use non-dashboard router
 app.use('/', router)
-
-// Use dashboard router (React-Router)
-app.use('/', dashboard)
 
 // Use non-dashboard router only for "/"
 app.get('/', (req, res) => {
