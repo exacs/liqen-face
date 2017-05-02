9e8e893178818296d593bca6ac9b0ead22717314
Carlos Saito <c.saito.style@gmail.com>
2017-05-02 23:29:12 +0200

    Fix Liqen.users.show

--- server/local-liqen.js
+++ server/local-liqen.js
@@ -6,7 +6,10 @@ const localLiqen = token => ({
   users: {
     show (id) {
       if (token === 'valid_token' && id === 1) {
-        return Promise.resolve()
+        return Promise.resolve({
+          id: 1,
+          email: 'john@example.com'
+        })
       } else {
         return Promise.reject(new Error('Not logged in'))
       }
