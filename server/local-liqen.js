3a5fb020d4076a3cea27b68249f836ad5fe83d46
Carlos Saito <c.saito.style@gmail.com>
2017-05-02 23:29:56 +0200

    Implement Client.sessions.create

--- server/local-liqen.js
+++ server/local-liqen.js
@@ -14,6 +14,22 @@ const localLiqen = token => ({
         return Promise.reject(new Error('Not logged in'))
       }
     }
+  },
+
+  sessions: {
+    create ({ email, password } = {}) {
+      if (email === 'john@example.com' && password === 'secret') {
+        return Promise.resolve({
+          access_token: 'valid_token',
+          expires: Math.floor(Date.now() / 1000 + 1000),
+          user: {
+            id: 1
+          }
+        })
+      } else {
+        return Promise.reject(new Error('not authenticated'))
+      }
+    }
   }
 })
 
