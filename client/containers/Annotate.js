caf63f16303f26db497943d31659fdeef482cee0
Carlos Saito <c.saito.style@gmail.com>
2017-06-14 13:18:46 +0200

    New component "annotator/Article"

--- client/containers/Annotate.js
+++ client/containers/Annotate.js
@@ -2,44 +2,10 @@ import React from 'react'
 import { connect } from 'react-redux'
 import zipWith from 'lodash/fp/zipWith'
 
-import Annotator from '../components/annotator'
+import Article from '../components/annotator/article'
 import Selector from '../components/annotator-drawer/selector'
 import LiqenCreator from '../components/annotator-drawer/liqen-creator'
 import { createAnnotation, createLiqen } from '../actions/index'
-
-// const question = JSON.parse(window.QUESTION)
-// const tags = question
-
-function convertObjectToReact (obj, key) {
-  if (typeof obj === 'string') {
-    return obj
-  } else {
-    const children = obj.children.map((item, i) => convertObjectToReact(item, i))
-
-    if (children.length === 1) {
-      return React.createElement(obj.name, Object.assign({key}, obj.attrs), children[0])
-    } else {
-      return React.createElement(obj.name, Object.assign({key}, obj.attrs), children)
-    }
-  }
-}
-
-const EncapsulatedArticle = ({ onCreateAnnotation, tags }) => (
-  <div>
-    {
-      JSON.parse(window.BODY_JSON).children.map((child, i) => (
-        <Annotator
-          key={i}
-          annotations={[]}
-          tags={tags}
-          onCreateAnnotation={onCreateAnnotation}
-        >
-          {convertObjectToReact(child)}
-        </Annotator>
-      ))
-    }
-  </div>
-)
 
 export function Annotate (
   {
@@ -58,16 +24,20 @@ export function Annotate (
           <LiqenCreator
             question={question}
             answer={answer}
-            onSubmit={onCreateLiqen} />
+            onSubmit={onCreateLiqen}
+          />
           <Selector
             annotations={annotations}
-            onSelect={(e) => console.log(e) }/>
+            onSelect={(e) => console.log(e)}
+          />
         </aside>
         <div className='col-lg-8 col-xl-7'>
           <main className='article-body'>
-            <EncapsulatedArticle
+            <Article
+              body={JSON.parse(window.BODY_JSON)}
               tags={tags}
-              onCreateAnnotation={onCreateAnnotation} />
+              onCreateAnnotation={onCreateAnnotation}
+            />
           </main>
         </div>
       </div>
