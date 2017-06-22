c8475f3b55e4ad4e127621b8f79e0dae2e209db6
Carlos Saito <c.saito.style@gmail.com>
2017-06-22 10:16:21 +0200

    Log failures and success responses

--- server/middlewares.js
+++ server/middlewares.js
@@ -8,13 +8,18 @@ export function checkSession (req, res, next) {
     return res.redirect('/login')
   }
 
+  console.log('Calling core.users.show')
   return req
     .core.users.show(userId)
     .then(user => {
+      console.log('Call success. Showing user: ', user)
       req.currentUser = user
       next()
     })
-    .catch(() => res.redirect('/login'))
+    .catch((e) => {
+      console.log('Call failed. Showing error: ', e)
+      res.redirect('/login')
+    })
 }
 
 export function login (req, res, next) {
@@ -24,12 +29,14 @@ export function login (req, res, next) {
 
   const cookies = new Cookies(req, res)
 
+  console.log('Calling core.sessions.create')
   return req
     .core.sessions.create({
       email: req.body.email,
       password: req.body.password
     })
     .then(session => {
+      console.log('Call success. Session returned')
       cookies
         .set('access_token', session.access_token, {httpOnly: false})
         .set('user_id', session.user.id, {httpOnly: false})
@@ -37,7 +44,10 @@ export function login (req, res, next) {
       req.user = session.user
       next()
     })
-    .catch(() => res.render('index'))
+    .catch((e) => {
+      console.log('Call failed. Showing error: ', e)
+      res.render('index')
+    })
 }
 
 export const setLiqenCore = core => (req, res, next) => {
