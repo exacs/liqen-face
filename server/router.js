a288515d5f80cb50a3f95b129b219b9763480482
Carlos Saito <c.saito.style@gmail.com>
2017-06-26 13:15:48 +0200

    Fix. Do not show if the annotation hasn't tags

--- server/router.js
+++ server/router.js
@@ -129,17 +129,20 @@ router.get('/annotate', checkSession, async function (req, res, next) {
       }))
 
       const annotations2 = {}
+      console.log(annotations)
 
       for (let annotation of annotations) {
-        annotations2[annotation.id] = {
-          tag: annotation.tags[0].id,
-          target: {
-            prefix: annotation.target.prefix,
-            exact: annotation.target.exact,
-            suffix: annotation.target.suffix
-          },
-          checked: false,
-          pending: false
+        if (annotation.tags.length > 0) {
+          annotations2[annotation.id] = {
+            tag: annotation.tags[0].id,
+            target: {
+              prefix: annotation.target.prefix,
+              exact: annotation.target.exact,
+              suffix: annotation.target.suffix
+            },
+            checked: false,
+            pending: false
+          }
         }
       }
 
